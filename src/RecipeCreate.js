8334f8a5d49533f3f763df228ec8e22755409f3b
michael <mstadnycki45@gmail.com>
2022-10-16 11:26:55 -0400

    update RecipeCreate.js

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,89 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({recipes, createRecipe}) {
+
+  const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [formData, setFormData] = useState({...initialFormState});
+
+  const onChange = ({target}) => setFormData({...formData, [target.name]: target.value});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createRecipe(formData);
+
+    setFormData({...initialFormState});
+  }
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
   
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <input
+                onChange={onChange}
+                value={formData.name}
+                id="name"
+                type="text"
+                placeholder="Name"
+                required={true}
+              />
+            </td>
+            <td>
+              <input
+                onChange={onChange}
+                value={formData.cuisine}
+                id="cuisine"
+                name="cuisine"
+                type="text"
+                placeholder="Cuisine"
+                required={true}
+              />
+            </td>
+            <td>
+              <input
+                onChange={onChange}
+                value={formData.photo}
+                id="photo"
+                name="photo"
+                type="url"
+                placeholder="URL"
+                required={true}
+              />
+            </td>
+            <td>
+              <textarea
+                onChange={onChange}
+                value={formData.ingredients}
+                id="ingredients"
+                name="ingredients"
+                placeholder="Ingredients"
+                required={true}
+                rows={3}
+              />
+            </td>
+            <td>
+              <textarea
+                onChange={onChange}
+                value={formData.preparation}
+                id="preparation"
+                name="preparation"
+                placeholder="Preparation"
+                required={true}
+                rows={3}
+              />
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
